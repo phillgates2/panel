d94d782ab5b4fa140120e15b05b39d093b662c23
phillgates2 <phillgates2@gmail.com>
2025-11-20 01:55:24 +0000

    style: Please enter a commit message to explain why this merge is necessary,

--- database_admin.py
+++ database_admin.py
@@ -107,12 +107,15 @@ class DatabaseAdmin:
                        WHERE table_name = %s AND table_schema = 'public'"""
             return self.execute_query(query, (table_name,))
         else:
-            return self.execute_query(f"PRAGMA table_info(`{table_name}`)")
+                return self.execute_query(f"PRAGMA table_info('{table_name}')")
 
     def get_table_data(self, table_name, limit=100, offset=0):
         """Get table data with pagination"""
-        query = f"SELECT * FROM `{table_name}` LIMIT {limit} OFFSET {offset}"
-        return self.execute_query(query)
+            if self.is_postgres():
+                query = f'SELECT * FROM "{table_name}" LIMIT {limit} OFFSET {offset}'
+            else:
+                query = f"SELECT * FROM '{table_name}' LIMIT {limit} OFFSET {offset}"
+            return self.execute_query(query)
 
     def get_database_info(self):
         """Get database information"""
@@ -187,7 +190,7 @@ DATABASE_ADMIN_BASE_TEMPLATE = """
     </div>
     <div class="container">
         <div class="nav-links">
-            <a href="{{ url_for('admin_db_home') }}">🏠 Home</a>
+            <a href="/">🏠 Home</a>
             <a href="{{ url_for('admin_db_query') }}">🔍 SQL Query</a>
             <a href="{{ url_for('admin_db_export') }}">📤 Export</a>
             <a href="{{ url_for('admin_db_import') }}">📥 Import</a>
